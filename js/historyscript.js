f5d7a26db3b17eb2030aac2df758fbc3811a2373
mrurban <ekene.ebulue@gmail.com>
2016-05-30 19:55:06 +0200

    BIU

---
+++ js/historyscript.js
@@ -0,0 +1,11 @@
+/**drop-down toggle**/
+jQuery('body').bind('click', function(e) {
+    if(jQuery(e.target).closest('.navbar').length == 0) {
+        // click happened outside of .navbar, so hide
+        var opened = jQuery('.navbar-collapse').hasClass('collapse in');
+        if ( opened === true ) {
+            jQuery('.navbar-collapse').collapse('hide');
+        }
+    }
+});
+
